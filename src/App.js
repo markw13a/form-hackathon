7f59c5d2a334515dfc09a940b19da417598359a8
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-01-28 13:24:30 +0000

    Styling applied to Header & Form Name section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './style/main.scss';
 
 const App = () => (
 	<div className="App">
+		<header><h1>Chameleon Form Builder</h1></header>
 		<div className="content">
 			<FormProvider>
 				<FormTitleCapture />
